7191759bbbebfded5785b5ea01f7b1370531b2df
Kian Dadkhah <contact.kiands@gmail.com>
2023-10-07 18:23:00 -0400

    Updated InputManager to use event-driven implementation instead of polling-driven

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -2,30 +2,42 @@
 // Author: Kian Dadkhah
 
 #include "InputManager.h"
+#include <iostream>
 
 namespace BombasticEngine {
 
   // Constructor implementation.
-  InputManager::InputManager() {
-    // Get the state of each key and the number of keys.
-    keyState = SDL_GetKeyboardState(&numKeys);
-  }
+  InputManager::InputManager() {}
 
   // Destructor implementation.
-  InputManager::~InputManager() {
-    // Nothing to do here.
-  }
+  InputManager::~InputManager() {}
 
   // Update implementation.
   void InputManager::update() {
-    // SDL_PumpEvents updates the keyState array with the current state of each key.
-    SDL_PumpEvents();
+    // Poll for events. If there is an event, process it.
+    while (SDL_PollEvent(&event)) {
+      std::cout << "Event polled: " << event.type << std::endl; // Debugging line
+
+      // If the event is a key press, update the key map.
+      if (event.type == SDL_KEYDOWN) {
+        keyMap[event.key.keysym.scancode] = true;
+      }
+      // If the event is a key release, update the key map.
+      if (event.type == SDL_KEYUP) {
+        keyMap[event.key.keysym.scancode] = false;
+      }
+    }
   }
 
   // isKeyPressed implementation.
   bool InputManager::isKeyPressed(SDL_Scancode key) const {
-    // Return the state of the specified key.
-    return keyState[key];
+    auto it = keyMap.find(key);
+    // If the key is found in the map, return its state.
+    if (it != keyMap.end()) {
+      return it->second;
+    }
+    // Otherwise, return false.
+    return false;
   }
 
 } // namespace BombasticEngine
