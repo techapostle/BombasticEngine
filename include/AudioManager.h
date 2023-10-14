1dd963951e1707722724ed417792833e155b691a
Kian Dadkhah <contact.kiands@gmail.com>
2023-10-14 19:46:03 -0400

    Implemented AudioManager. Currently only supports WAV files. Untested.

---
+++ include/AudioManager.h
@@ -0,0 +1,126 @@
+// include/InputManager.h
+
+#pragma once
+
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <string>
+#include <map>
+
+namespace BombasticEngine {
+  class AudioManager {
+    public:
+      /**
+       * @brief Construct a new Audio Manager object
+      */
+      AudioManager();
+      /**
+       * @brief Destroy the Audio Manager object
+      */
+      ~AudioManager();
+
+      /**
+       * @brief Initialize the audio manager.
+       * 
+       * @return true 
+       * @return false 
+       */
+      bool initialize();
+      /**
+       * @brief Load a sound file into the audio manager.
+       * 
+       * @param id The id of the sound.
+       * @param filePath The file path to the sound file.
+      */
+      bool loadSound(const std::string& id, const std::string& filePath);
+      /**
+       * @brief Play a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+       * @param loop Whether the sound should loop or not.
+      */
+      void playSound(const std::string& id, bool loop = false);
+      /**
+       * @brief Stop a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+      */
+      void stopSound(const std::string& id);
+      /**
+       * @brief Pause a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+      */
+      void pauseSound(const std::string& id);
+
+      /**
+       * @brief Set the volume of a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+       * @param volume The volume of the sound.
+      */
+      void setVolume(const std::string& id, float volume); // volume range: 0.0 (mute) to 1.0 (max)
+      /**
+       * @brief Set the pitch of a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+       * @param pitch The pitch of the sound.
+      */
+      void setPitch(const std::string& id, float pitch);   // default pitch is 1.0
+      /**
+       * @brief Set the position of a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+       * @param x The x position of the sound.
+       * @param y The y position of the sound.
+       * @param z The z position of the sound.
+      */
+      void setPosition(const std::string& id, float x, float y, float z);
+      /**
+       * @brief Set the velocity of a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+       * @param x The x velocity of the sound.
+       * @param y The y velocity of the sound.
+       * @param z The z velocity of the sound.
+      */
+      void setVelocity(const std::string& id, float x, float y, float z);
+      /**
+       * @brief Set the orientation of a loaded sound with the specified id.
+       * 
+       * @param id The id of the sound.
+       * @param atX The x position of the sound.
+       * @param atY The y position of the sound.
+       * @param atZ The z position of the sound.
+       * @param upX The x up vector of the sound.
+       * @param upY The y up vector of the sound.
+       * @param upZ The z up vector of the sound.
+      */
+      void setListenerPosition(float x, float y, float z);
+      /**
+       * @brief Set the velocity of the listener.
+       * 
+       * @param x The x velocity of the listener.
+       * @param y The y velocity of the listener.
+       * @param z The z velocity of the listener.
+      */
+      void setListenerVelocity(float x, float y, float z);
+      /**
+       * @brief Set the orientation of the listener.
+       * 
+       * @param atX The x position of the listener.
+       * @param atY The y position of the listener.
+       * @param atZ The z position of the listener.
+       * @param upX The x up vector of the listener.
+       * @param upY The y up vector of the listener.
+       * @param upZ The z up vector of the listener.
+      */
+      void setListenerOrientation(float atX, float atY, float atZ, float upX, float upY, float upZ);
+
+    private:
+      ALCdevice* device; // The audio device.
+      ALCcontext* context; // The audio context.
+      std::map<std::string, ALuint> soundBuffers; // Map of sound ids to their corresponding buffers.
+      std::map<std::string, ALuint> soundSources; // Map of sound ids to their corresponding sources.
+  };
+} // namespace BombasticEngine
---
+++ src/AudioManager.cpp
@@ -0,0 +1,136 @@
+// src/AudioManager.cpp
+
+#include "AudioManager.h"
+#include <iostream>
+#include <AL/alu.h>
+
+namespace BombasticEngine {
+
+  // Constructor implementation.
+  AudioManager::AudioManager() : device(nullptr), context(nullptr) {}
+
+  // Destructor implementation.
+  AudioManager::~AudioManager() {
+    // Cleanup resources.
+    for (auto& pair : soundSources) {
+      alDeleteSources(1, &pair.second);
+    }
+    for (auto& pair : soundBuffers) {
+      alDeleteBuffers(1, &pair.second);
+    }
+    alcMakeContextCurrent(nullptr);
+    alcDestroyContext(context);
+    alcCloseDevice(device);
+  }
+
+  // Initialize implementation.
+  bool AudioManager::initialize() {
+    device = alcOpenDevice(nullptr);
+    if (!device) {
+      std::cerr << "Failed to open OpenAL device!" << std::endl;
+      return false;
+    }
+    context = alcCreateContext(device, nullptr);
+    if (!context) {
+      std::cerr << "Failed to create OpenAL context!" << std::endl;
+      return false;
+    }
+    alcMakeContextCurrent(context);
+    return true;
+  }
+
+  // loadSound implementation.
+  bool AudioManager::loadSound(const std::string& id, const std::string& filePath) {
+    ALuint buffer;
+    ALenum format;
+    ALvoid* data;
+    ALsizei size;
+    ALsizei freq;
+    ALboolean loop;
+
+    // Load WAV file using ALUT.
+    alGenBuffers(1, &buffer);
+    if (alGetError() != AL_NO_ERROR) {
+      std::cerr << "Failed to generate OpenAL buffer!" << std::endl;
+      return false;
+    }
+
+    data = alutLoadMemoryFromFile(filePath.c_str(), &format, &size, &freq);
+    if (!data) {
+      std::cerr << "Failed to load sound file " << filePath << "!" << std::endl;
+      return false;
+    }
+
+    alBufferData(buffer, format, data, size, freq);
+    alutUnloadMemory(data, size, freq);
+
+    // Generate a source for the sound.
+    ALuint source;
+    alGenSources(1, &source);
+    alSourcei(source, AL_BUFFER, buffer);
+
+    soundBuffers[id] = buffer;
+    soundSources[id] = source;
+
+    return true;
+  }
+
+  // playSound implementation.
+  void AudioManager::playSound(const std::string& id, bool loop) {
+    ALuint source = soundSources[id];
+    alSourcei(source, AL_LOOPING, loop ? AL_TRUE : AL_FALSE);
+    alSourcePlay(source);
+  }
+
+  // stopSound implementation.
+  void AudioManager::stopSound(const std::string& id) {
+    ALuint source = soundSources[id];
+    alSourceStop(source);
+  }
+
+  // pauseSound implementation.
+  void AudioManager::pauseSound(const std::string& id) {
+    ALuint source = soundSources[id];
+    alSourcePause(source);
+  }
+
+  // setVolume implementation.
+  void AudioManager::setVolume(const std::string& id, float volume) {
+    ALuint source = soundSources[id];
+    alSourcef(source, AL_GAIN, volume);
+  }
+
+  // setPitch implementation.
+  void AudioManager::setPitch(const std::string& id, float pitch) {
+    ALuint source = soundSources[id];
+    alSourcef(source, AL_PITCH, pitch);
+  }
+
+  // setPosition implementation.
+  void AudioManager::setPosition(const std::string& id, float x, float y, float z) {
+    ALuint source = soundSources[id];
+    alSource3f(source, AL_POSITION, x, y, z);
+  }
+
+  // setVelocity implementation.
+  void AudioManager::setVelocity(const std::string& id, float x, float y, float z) {
+    ALuint source = soundSources[id];
+    alSource3f(source, AL_VELOCITY, x, y, z);
+  }
+
+  // setListenerPosition implementation.
+  void AudioManager::setListenerPosition(float x, float y, float z) {
+    alListener3f(AL_POSITION, x, y, z);
+  }
+
+  // setListenerVelocity implementation.
+  void AudioManager::setListenerVelocity(float x, float y, float z) {
+    alListener3f(AL_VELOCITY, x, y, z);
+  }
+
+  // setListenerOrientation implementation.
+  void AudioManager::setListenerOrientation(float x, float y, float z) {
+    alListener3f(AL_ORIENTATION, x, y, z);
+  }
+
+} // namespace BombasticEngine
